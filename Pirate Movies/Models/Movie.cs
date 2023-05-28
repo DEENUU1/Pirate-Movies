a69bd02515dd9a54962dc03e6600f50f579c10ac
Kacper Włodarczyk <kacperwlodarczyk@protonmail.com>
2023-05-28 15:43:41 +0200

    Feature | Movie model |
    Model movie which represent a movie, has few fields
    Didnt implemented Poster and Links fields

---
+++ Pirate Movies/Models/Movie.cs
@@ -0,0 +1,14 @@
+﻿namespace Pirate_Movies.Models
+{
+    public class Movie
+    {
+        public Guid Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        // Poster 
+        public DateOnly DateRelease { get; set; }
+        public string Country { get; set; }
+        public Category Category { get; set; }
+        // Links Collections
+    }
+}
