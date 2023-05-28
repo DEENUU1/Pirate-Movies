1e92563d22cc5da8d92d2dba25e04777f76cbf01
Kacper Włodarczyk <kacperwlodarczyk@protonmail.com>
2023-05-28 15:51:04 +0200

    Feature | Episode model |
    Episode model which can represent a episode for Show model
    Has a few fields but not implemented Links ICollections

---
+++ Pirate Movies/Models/Episode.cs
@@ -0,0 +1,12 @@
+﻿namespace Pirate_Movies.Models
+{
+    public class Episode
+    {
+        public Guid Id { get; set; }
+        public int Number { get; set; }
+        public string Title { get; set; }
+        // Links
+        public Show ShowId { get; set; }
+        public Season Season { get; set; }
+    }
+}
