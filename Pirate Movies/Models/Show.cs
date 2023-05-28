0b9779f30420a594763a3ee792ec6a05ff625624
Kacper Włodarczyk <kacperwlodarczyk@protonmail.com>
2023-05-28 15:46:19 +0200

    Feature | Show model |
    Show model to represent a tv series
    Didnt implemented Season and Poster fields

---
+++ Pirate Movies/Models/Show.cs
@@ -0,0 +1,14 @@
+﻿namespace Pirate_Movies.Models
+{
+    public class Show
+    {
+        public Guid Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        // poster
+        public DateOnly DateRelease { get; set; }
+        public string Country { get; set; }
+        public Category Category { get; set; } 
+        // Season Id 
+    }
+}
