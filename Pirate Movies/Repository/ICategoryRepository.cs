c7809532e8110dce6efc4567e6fcbedde470a4c5
Kacper Włodarczyk <kacperwlodarczyk@protonmail.com>
2023-05-29 16:26:31 +0200

    Feature | Repository patterns and Interface for Category

---
+++ Pirate Movies/Repository/ICategoryRepository.cs
@@ -0,0 +1,10 @@
+﻿using Pirate_Movies.Models;
+
+namespace Pirate_Movies.Repository
+{
+    public interface ICategoryRepository
+    {
+        ICollection<Category> GetCategories();
+        Category GetCategory(int id);
+    }
+}
