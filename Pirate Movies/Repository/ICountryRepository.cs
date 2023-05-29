a452824480561bdea8f57b02c1f194d912d7f30d
Kacper Włodarczyk <kacperwlodarczyk@protonmail.com>
2023-05-29 16:27:42 +0200

    Feature | Country Repository Interface |
    Get all countries
    Get country based on ID

---
+++ Pirate Movies/Repository/ICountryRepository.cs
@@ -0,0 +1,10 @@
+﻿using Pirate_Movies.Models;
+
+namespace Pirate_Movies.Repository
+{
+    public interface ICountryRepository
+    {
+        ICollection<Country> GetCountries();
+        Country GetCountry(int id);
+    }
+}
