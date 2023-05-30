d6f89710b719518f3d9c35fbf88e9c565757e03e
Kacper Włodarczyk <kacperwlodarczyk@protonmail.com>
2023-05-30 15:06:59 +0200

    Feature | Implementation of ICountryRepository interface |
    Delete, Create, Update and more function for CRUD operations on Country model

---
+++ Pirate Movies/Repository/CountryRepository.cs
@@ -0,0 +1,55 @@
+﻿using Pirate_Movies.Data;
+using Pirate_Movies.Interfaces;
+using Pirate_Movies.Models;
+
+namespace Pirate_Movies.Repository
+{
+    public class CountryRepository : ICountryRepository
+    {
+        private DataContext _context;
+
+        public CountryRepository(DataContext context)
+        {
+            _context = context;
+        }
+
+        public bool CountryExists(int id)
+        {
+            return _context.Countries.Any(c => c.Id == id);
+        }
+
+        public bool CreateCountry(Country country)
+        {
+            _context.Add(country);
+            return Save();
+        }
+
+        public bool DeleteCountry(Country country)
+        {
+            _context.Remove(country);
+            return Save();
+        }
+
+        public ICollection<Country> GetCountries()
+        {
+            return _context.Countries.ToList();
+        }
+
+        public Country GetCountry(int id)
+        {
+            return _context.Countries.Where(c => c.Id == id).FirstOrDefault();
+        }
+
+        public bool Save()
+        {
+            var saved = _context.SaveChanges();
+            return saved > 0 ? true : false;
+        }
+
+        public bool UpdateCountry(Country country)
+        {
+            _context.Update(country);
+            return Save();
+        }
+    }
+}
